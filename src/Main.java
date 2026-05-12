94930c46a20b8f26f2be71267b785ba9cf5d5b56
FelipeSena1291 <felipesena1291@outlook.com>
2026-05-12 16:01:27 -0300

    arquivos

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class FuncionarioPadrao{
+//    String nome, matricula;
+}
+
+
+public class Main{
+    public static void main (String [] args ){
+        Scanner letor = new Scanner(System.in);
+
+
+    }
+
+}
