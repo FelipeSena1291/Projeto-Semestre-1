34399aecf5a42f4edeaf452ff0de2d57b3ead0cb
FelipeSena1291 <felipesena1291@outlook.com>
2026-05-13 16:31:40 -0300

    Estruturas de escolhas conluida

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Projeto-Semestre-1
-O projeto do primeiro semestre consiste em criar um codigo, onde seja possivel cadastrar funcionarios de uma determinada empresa, pode ser o funcionario padrão, por comissão ou por produção
+O projeto do primeiro semestre consiste em criar um codigo, onde seja possivel cadastrar funcionarios de uma 
+determinada empresa, pode ser o funcionario padrão, por comissão ou por produção.
+
+Menu sera exibido informando
+ Sistema de cadastramento de Funcionarios
+[1] - Cadastrar funcionario Padrão
+[2] - Cadastrar funcionario Comissionado
+[3] - Cadastrar funcionario Produção
+[4] - Gerar folha do todos Funcioanrios
+[0] - Sair do Sistema
+
+Funcionario padrao = recebe nome, matricula e salario.
+
+Funcionario Comissionado = recebe nome matricula ,salario, quantidade de peças vendidas e a porcentagem da comissão,
+que sera adicionada ao salario final
+
+Funcionario Procução = recebe  nome, matricula ,salario, quantidade de peças produzidas e um valor fixo por cada peça
+sera adicionado ao salario final
--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 class Funcionario{
-      String nome;
-      int id, matricula;
-      double salarioFinal;
+    String nome;
+    int id, matricula;
+    double salarioFinal;
 
-      public Funcionario (int id,String nome, int matricula,double salarioFinal){
-          this.nome = nome;
-          this.id = id;
-          this.matricula = matricula;
-          this.salarioFinal = salarioFinal;
-      }
-      public void exibirDados(){
-          System.out.println("ID: " + id);
-          System.out.println("Nome: " + nome);
-          System.out.println("Matricula: " + matricula);
-          System.out.println("Salario: " + salarioFinal);
-      }
+    public Funcionario (int id,String nome, int matricula,double salarioFinal){
+        this.nome = nome;
+        this.id = id;
+        this.matricula = matricula;
+        this.salarioFinal = salarioFinal;
+    }
+    public void exibirDados(){
+        System.out.println("ID: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Salario: " + salarioFinal);
+    }
 
 }
 class FuncionarioComisao extends Funcionario {
     double valorDeVendas,comissao;
 
     public FuncionarioComisao (int id, String nome,int matricula,double salarioFinal,double valorDeVendas,double comissao){
-        super(id, nome, matricula, salarioFinal);
+        super(id, nome,matricula,salarioFinal);
         this.valorDeVendas = valorDeVendas;
         this.comissao = comissao;
     }
@@ -34,6 +34,7 @@ class FuncionarioComisao extends Funcionario {
         super.exibirDados();
         System.out.println("Valor de Venda: " + valorDeVendas);
         System.out.println("Valor da Comissão: " + comissao);
+        System.out.println("Salario Final: " + ( salarioFinal+(valorDeVendas*comissao)/100));
     }
 
 }
@@ -50,6 +51,7 @@ class FuncionarioProducao extends Funcionario {
         super.exibirDados();
         System.out.print("Quantidade de peças vendidas: " + quantidadeDePecasVendidas);
         System.out.println("Valor da peça: " + valorDaPeca);
+        System.out.println("Salario Final: " + ( salarioFinal+(quantidadeDePecasVendidas * valorDaPeca)));
     }
 }
 
@@ -83,18 +85,80 @@ public class Main{
                 }
             } catch(InputMismatchException e) {
 
-                    System.out.println("Erro: Digite uma opção valida");
-                    entrada.nextLine();
+                System.out.println("Erro: Digite uma opção valida");
+                entrada.nextLine();
 
-                    opcao = -1;
+                opcao = -1;
             }
 
             switch (opcao) {
                 case 1:
+                    entrada.nextLine();
+
+                    System.out.print("Digite o nome do funcionario :");
+                    String nome = entrada.nextLine();
+
+                    System.out.print("Informe a Matricula: ");
+                    int matricula = entrada.nextInt();
+
+                    System.out.print("Informe o Salario: ");
+                    double salario = testarValoresNegativos(entrada);
+
+                    contador++;
+
+                    funcionarios.add(new Funcionario(contador,nome,matricula,salario));
+                    break;
                 case 2:
+                    entrada.nextLine();
+
+                    System.out.print("Digite o nome do funcionario :");
+                    String nomeComissao = entrada.nextLine();
+
+                    System.out.print("Informe a Matricula: ");
+                    int matriculaComissao = entrada.nextInt();
+
+                    System.out.print("Informe o Salario: ");
+                    double salarioComissao = testarValoresNegativos(entrada);
+
+                    System.out.println("Informe o valor total de vendas: ");
+                    double vendasTotal = testarValoresNegativos(entrada);
+
+                    System.out.println("Valor da comissao (%): ");
+                    double comissao = testarValoresNegativos(entrada);
+
+                    entrada.nextLine();
+                    contador++;
+
+                    funcionarios.add(new FuncionarioComisao(contador, nomeComissao, matriculaComissao,salarioComissao, vendasTotal, comissao));
+                    break;
                 case 3:
+                    entrada.nextLine();
+
+                    System.out.print("Digite o nome do funcionario :");
+                    String nomeProducao = entrada.nextLine();
+
+                    System.out.print("Informe a Matricula: ");
+                    int matriculaProducao = entrada.nextInt();
+
+                    System.out.print("Informe o Salario: ");
+                    double salarioProducao = testarValoresNegativos(entrada);
+
+                    System.out.println("Informe quantidade de peças produzidas: ");
+                    double vendasProducao = testarValoresNegativos(entrada);
+
+                    System.out.println("Valor por cada producao: ");
+                    double Producao = testarValoresNegativos(entrada);
+
+                    entrada.nextLine();
+                    contador++;
+
+                    funcionarios.add(new FuncionarioProducao(contador, nomeProducao, matriculaProducao,salarioProducao, vendasProducao, Producao));
+
+                    break;
+
                 case 4:
                     for(Funcionario f: funcionarios){
+                        System.out.println("=======================");
                         f.exibirDados();
 
                     }
@@ -102,6 +166,18 @@ public class Main{
         } while (opcao != 0);
         System.out.print("Obrigado por utilizar");
 
+
+    }
+
+    public static double testarValoresNegativos(Scanner entrada){
+        double valor = entrada.nextDouble();
+
+        while (valor <0) {
+            System.out.print("Informe um valor valido");
+            valor = entrada.nextDouble();
+        }
+        return valor;
     }
 
 }
+
